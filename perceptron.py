2f09b81bb412f9a7b910ffe789ffa0fbe2dfdc80
rmngllnn <romane.gllnn@gmail.com>
2020-12-21 11:19:34 +0100

    Ajout de commentaires

--- perceptron.py
+++ perceptron.py
@@ -9,8 +9,10 @@
 # https://becominghuman.ai/part-of-speech-tagging-tutorial-with-the-keras-deep-learning-library-d7f93fa05537 (list of features)
 
 import random
+import time
 
-def get_word_vector(sentence, word, index): #hypothesis: word, word before, word after, pre- and suffixes with len from 1 to 3, are enough to determine POS tagging
+def get_word_vector(sentence, word, index): 
+	#hypothesis: word, word before, word after, pre- and suffixes with len from 1 to 3, are enough to determine POS tagging
 	vector = {}
 
 	if index == 0:
@@ -34,7 +36,7 @@ def get_word_vector(sentence, word, index): #hypothesis: word, word before, word
 		word_plus_1 = sentence[index+1]
 	vector["word-1="+word_plus_1] = 1
 
-	#RAF determine if "", then still a feature, or if then = 0
+	#To do : determine if "", then still a feature, or if then = 0
 	prefix_1 = ""
 	if len(word) > 0:
 		prefix_1 = word[0:1]
@@ -64,6 +66,8 @@ def get_word_vector(sentence, word, index): #hypothesis: word, word before, word
 	if len(word) > 2:
 		suffix_3 = word[0:3]
 	vector["suffix3="+suffix_3] = 1
+	
+	#print(vector)
 
 	return vector
 
@@ -96,6 +100,9 @@ def add_weights_to_average(average, weights):
 
 
 def train(vectors_corpus, tagset, MAX_EPOCH = 1):
+	
+	"""création du dictionnaire train
+	"""
 	average = {}
 
 	for epoch in range(0, MAX_EPOCH):
@@ -105,7 +112,7 @@ def train(vectors_corpus, tagset, MAX_EPOCH = 1):
 		index_word = 0
 		n_words = len(vectors_corpus)
 		for word in vectors_corpus:
-			print("epoch "+str(epoch)+"/"+str(MAX_EPOCH)+": "+str(index_word)+"/"+str(n_words)+" words") #TBD
+			#print("epoch "+str(epoch)+"/"+str(MAX_EPOCH)+": "+str(index_word)+"/"+str(n_words)+" words") #TBD
 			index_word += 1
 			predicted_tag = predict_tag(word[0], weights, tagset)
 			gold_tag = word[1]
@@ -113,12 +120,19 @@ def train(vectors_corpus, tagset, MAX_EPOCH = 1):
 				add_vector_to_weights(word[0], weights, predicted_tag, -1)
 				add_vector_to_weights(word[0], weights, gold_tag, +1)
 			add_weights_to_average(average, weights)
-	print(average) # TBD
+	#print(average) # TBD
 	return average
 
 
 
 def get_data_from_file(file = "./fr_gsd-ud-train.conllu"):
+	
+	"""extraction des données à partir des fichiers conllu
+	   return : list de dictionnaires 
+       (exemple pour une phrase : 
+		[{index du mot dans la phrase : 1, mot : mot1, gold_pos : ADV}, 
+         {index du mot dans la phrase 2, mot : mot2, gold_pos : DET}, etc."""
+	
 	data = [] # list of lists (sentences) of dictionaries (words)
 
 	with open(file, "r") as raw_file:
@@ -136,10 +150,16 @@ def get_data_from_file(file = "./fr_gsd-ud-train.conllu"):
 						to_append["gold_POS"] = tabs[3]
 						sentence_data.append(to_append)
 			data.append(sentence_data)
+	
 	return data
 
 
-def get_vectors_from_data(data): # vectors: list of tuples, with (word vector, gold POS tag)
+def get_vectors_from_data(data):
+	
+	"""Création des vecteurs à partir des données extraites du fichiers
+	   return : list of tuples""" 
+	
+	# vectors: list of tuples, with (word vector, gold POS tag)
 	vectors = []
 	for sentence_data in data:
 		sentence = []
@@ -153,8 +173,12 @@ def get_vectors_from_data(data): # vectors: list of tuples, with (word vector, g
 
 
 if "__main__" == __name__:
+	
+	start_time = time.time()
+	
 	#train_data = get_data_from_file("./fr_gsd-ud-train.conllu")
 	dev_data = get_data_from_file("./fr_gsd-ud-dev.conllu")
+	
 	#test_data = get_data_from_file("./fr_gsd-ud-test.conllu")
 
 	#train_vectors = get_vectors_from_data(train_data)
@@ -166,6 +190,8 @@ if "__main__" == __name__:
 
 	weigths = {} #donc que des 0 
 	weights = train(dev_vectors, tagset) #weights[feature][tag]
+	
+	print(int(time.time()-start_time), " secondes")
 
-	#RAF validation with MAX_EPOCH and dev_vectors
-	#RAF evaluation with test_vectors
+	#To do : validation with MAX_EPOCH and dev_vectors
+	#To do : evaluation with test_vectors
