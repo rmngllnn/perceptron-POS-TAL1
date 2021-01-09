5ccdfbf7f80230ab9fdd3cabc5e8d90c08c4089c
CGuitel <76062406+CGuitel@users.noreply.github.com>
2021-01-09 19:04:21 +0100

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Projet POs-Tagging with an averaged perceptron
+Project: POS-Tagging with an averaged perceptron
 @authors: GALLIENNE Romane, GUITEL Cécile
+
+corpus:
+https://universaldependencies.org/treebanks/fr_gsd/index.html#ud-french-gs
+https://github.com/UniversalDependencies/UD_French-GSD
+
+bibliography: 
+https://moodle.u-paris.fr/pluginfile.php/649525/mod_resource/content/1/perceptron.pdf (training pseudo-code algorithm)
+https://www.nltk.org/_modules/nltk/tag/perceptron.html
+https://www.guru99.com/pos-tagging-chunking-nltk.html
+https://becominghuman.ai/part-of-speech-tagging-tutorial-with-the-keras-deep-learning-library-d7f93fa05537 (list of features)
 """
-
-
-# corpus:
-# https://universaldependencies.org/treebanks/fr_gsd/index.html#ud-french-gs
-# https://github.com/UniversalDependencies/UD_French-GSD
-
-# bibliography: 
-# https://moodle.u-paris.fr/pluginfile.php/649525/mod_resource/content/1/perceptron.pdf (training pseudo-code algorithm)
-# https://www.nltk.org/_modules/nltk/tag/perceptron.html
-# https://www.guru99.com/pos-tagging-chunking-nltk.html
-# https://becominghuman.ai/part-of-speech-tagging-tutorial-with-the-keras-deep-learning-library-d7f93fa05537 (list of features)
 
 import random
 import time
@@ -29,9 +28,10 @@ import pickle
 def get_data_from_file(file = "./fr_gsd-ud-train.conllu"):
 	"""Extracts and returns the data from a conllu file. Formats them in 
 	a list of lists (sentences) of dictionnaries (words).
+
 	Example:
 	[[{index: 1, word: sentence1word1, gold_POS : ADV}, 
-   {index: 2, word : sentence1word2, gold_POS : DET}, ...],
+	  {index: 2, word : sentence1word2, gold_POS : DET}, ...],
 	 [{index:1; word: sentence2word1, ...}, ...],
 	 ...]
 
@@ -137,19 +137,6 @@ def get_word_vector(sentence, word, index):
 		vector["prefix3="+word[0:3]] = 1
 		vector["suffix3="+word[-3::]] = 1
 
-	"""
-	if len_word > 0:
-		vector["suffix1="+word[-1::]] = 1
-
-	if len_word > 1:
-		vector["suffix2="+word[-2::]] = 1
-
-	if len_word > 2:
-		vector["suffix3="+word[-3::]] = 1
-	"""
-
-	#print(vector)
-
 	return vector
 
 
@@ -212,7 +199,7 @@ def add_weights_to_average(average, weights):
 
 
 
-def train(train_vectors, tag_list, MAX_EPOCH = 1, evaluate_epochs = False, dev_vectors = None):
+def train(train_vectors, tag_list, MAX_EPOCH = 10, evaluate_epochs = False, dev_vectors = None):
 	"""Creates and return the weights to score each tags and predict the best 
 	one. Weights are averaged by adding each temporary value of them to each
 	other.
@@ -250,8 +237,8 @@ def train(train_vectors, tag_list, MAX_EPOCH = 1, evaluate_epochs = False, dev_v
 			if predicted_tag != gold_tag:
 				add_vector_to_weights(word[0], weights, predicted_tag, -1)
 				add_vector_to_weights(word[0], weights, gold_tag, +1)
-		#RAF la ligne suivante est dans la seconde boucle for dans le pseudo-code du prof, ?
-		add_weights_to_average(average, weights)
+		#RAF la ligne suivante est dans cette boucle for dans le pseudo-code du prof, ?, c'est celle-là dont je parlais
+			add_weights_to_average(average, weights)
 
 		if evaluate_epochs:
 			accuracy_by_epoch[epoch] = evaluate_accuracy(get_decision_corpus(weights, dev_vectors, tag_list))
@@ -361,46 +348,90 @@ def get_decision_corpus(weights, test_vectors, tag_list): #RAF comment/si ajoute
 	return list_freq"""
 
 
+def get_vocabulary(words):
+	"""Creates and return a dictionary of known word vectors.
 
-def evaluate_accuracy(decision_corpus):
+	words: list of (word_vector, gold_tag) as created/formatted by
+	get_vectors_from_data()
+	"""
+
+	vocabulary = []
+	for word in words:
+		vocabulary.append(word[0])
+
+	return vocabulary
+
+
+def evaluate_accuracy(decision_corpus, vocabulary):
 	"""Calculates, prints and returns the number of good guesses.
-	Prints the errors."""
+	Prints the errors.
 
-	good = 0
+	decision_corpus: list of tagging decisions, saved as dictionaries (word_vec, pred_pos,
+	gold_pos), as created/formatted by get_decision_corpus()
+	vocabulary: list of all the word vectors the weights were trained on, created/formatted
+	by get_vocabulary
+	"""
+
+	good_overall = 0
+
+	good_known = 0
+	total_known = 0
+
 	for decision in decision_corpus:
-		if decision["gold_tag"] == decision["predicted_tag"]:
-			good += 1
+		#print(good_overall) #RAS
+		if decision["word_vector"] in vocabulary:
+			total_known += 1
 
-	print("Accuracy: "+str(good)+"/"+str(len(decision_corpus)))
-	return good
+		if decision["gold_tag"] == decision["predicted_tag"]:
+			good_overall += 1
+			if decision["word_vector"] in vocabulary:
+				good_known += 1
+
+	good_unknown = good_overall - good_known
+	total_overall = len(decision_corpus)
+	total_unknown = total_overall - total_known
+
+	print("Accuracy: "+str(good_overall)+"/"+str(total_overall)+" overall\t"+str(good_known)+"/"+str(total_known)+" known\t"+str(good_unknown)+"/"+str(total_unknown)+" unknown")
+	return good_overall
+
 
 
 def print_errors(decision_corpus):
-	"""Just prints the errors."""
+	"""Just prints the errors.
+
+	decision_corpus: list of tagging decisions, saved as dictionaries (word_vec, pred_pos,
+	gold_pos), as created/formatted by get_decision_corpus()
+	"""
 
 	for decision in decision_corpus:
 		if decision["gold_tag"] != decision["predicted_tag"]:
-			print(decision["word_vector"])
+			print(decision)
 			
 
-def serialisation_weight_vectors(weight_vector):
-	
-	"""save on desktop in a pickle file weight vectors to prevent from
-	   creating each time the weight vector"""
+def serialise_weights(weights):
+	"""Saves the weights in a pickle file to prevent from having to
+	retrain the perceptron every single time.
+
+	weights: the weights to be serialized, as created/formatted by train()
+	"""
     
-	file_name = "weight_vector.pkl"
-	f = open(file_name, "wb")
-	pickle.dump(weight_vector, f)
-	f.close()
+	file_name = "weights.pkl"
+	file = open(file_name, "wb")
+	pickle.dump(weights, file)
+	file.close()
 
-def deserialisation_weight_vectors():
-	data = open("weight_vector.pkl", 'rb') 
-	de = pickle.load(data)
-	print(de)
+
+
+def deserialise_weights():
+	"""Loads the weights from the serialised file on desktop.
+	"""
+    
+	data = open("weights.pkl", 'rb') 
+	weights = pickle.load(data)
 	data.close()
+
+	return weights
 			
-
-
 
 
 if "__main__" == __name__:
@@ -414,16 +445,18 @@ if "__main__" == __name__:
 
 	train_data_gsd = get_data_from_file("./fr_gsd-ud-train.conllu")
 	train_vectors_gsd = get_vectors_from_data(train_data_gsd)
-	weights = train(train_vectors_gsd, tag_list, MAX_EPOCH=50)
-	#weight_ser = serialisation_weight_vectors(weights)
-	#deserialisation_weight_vectors()
+	vocabulary = get_vocabulary(train_vectors_gsd)
+
+	weights = train(train_vectors_gsd, tag_list)
+	serialise_weights(weights)
+	#weights = deserialise_weights()
 	print()
 
 
 	"""print("Evaluating MAX_EPOCH")
 	dev_data = get_data_from_file("./fr_gsd-ud-dev.conllu")
 	dev_vectors = get_vectors_from_data(dev_data)
-	weights = train(dev_vectors, tag_list, MAX_EPOCH=100, evaluate_epochs=True, dev_vectors=dev_vectors)
+	weights = train(dev_vectors, tag_list, MAX_EPOCH=50, evaluate_epochs=True, dev_vectors=dev_vectors)
 	print()"""
 
 
@@ -433,11 +466,11 @@ if "__main__" == __name__:
 
 	print("On training data/known vocab")
 	decision_corpus_gsd_train = get_decision_corpus(weights, train_vectors_gsd, tag_list)
-	evaluate_accuracy(decision_corpus_gsd_train)
+	evaluate_accuracy(decision_corpus_gsd_train, vocabulary)
 
 	print("On testing data/new vocab")
 	decision_corpus_gsd_test = get_decision_corpus(weights, test_vectors_gsd, tag_list)
-	evaluate_accuracy(decision_corpus_gsd_test)
+	evaluate_accuracy(decision_corpus_gsd_test, vocabulary)
 	#get_confusion_matrix(decision_corpus_gsd_test, "evaluation in-domaine")
 	print()
 	
@@ -447,14 +480,14 @@ if "__main__" == __name__:
 	test_vectors_spoken = get_vectors_from_data(test_data_spoken)
 	print("Spoken data")
 	decision_corpus_spoken = get_decision_corpus(weights, test_vectors_spoken, tag_list)
-	evaluate_accuracy(decision_corpus_spoken)
+	evaluate_accuracy(decision_corpus_spoken, vocabulary)
 	print_errors(decision_corpus_spoken)
 
 	test_data_SRCMF = get_data_from_file("./Eval_HorsDomaine/French_SRCMF/fro_srcmf-ud-test.conllu")
 	test_vectors_SRCMF = get_vectors_from_data(test_data_SRCMF)
 	print("SRCMF data")
 	decision_corpus_SRCMF = get_decision_corpus(weights, test_vectors_SRCMF, tag_list)
-	evaluate_accuracy(decision_corpus_SRCMF)
+	evaluate_accuracy(decision_corpus_SRCMF, vocabulary)
 	print()
 
 
